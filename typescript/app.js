d99761d7de6f5d23b82b6c5df6a232ac8bb8e8b6
Daryl Olivares <dolivares@marsol.cl>
2021-07-13 17:15:50 -0400

    promesas 2

--- typescript/app.js
+++ typescript/app.js
@@ -1,17 +1,18 @@
 "use strict";
 (() => {
-    console.log('Inicio');
-    const prom1 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Se terminó el timeout');
-            //reject('Hubo un error');
-        }, 1000);
-    });
-    prom1.then(mensaje => console.log(mensaje)); //Devuelve la respuesta correcta
-    prom1.catch(err => console.warn(err)); //Devuelve el error
-    //Puede ser realizado igualmente de la siguiente forma:
-    prom1
-        .then(mensaje => console.log(mensaje))
-        .catch(err => console.warn(err));
-    console.log('Final');
+    const retirarDinero = (montoRetirar) => {
+        let dineroActual = 1000;
+        return new Promise((resolve, reject) => {
+            if (montoRetirar > dineroActual) {
+                reject('No hay fondos suficientes');
+            }
+            else {
+                dineroActual -= montoRetirar;
+                resolve(dineroActual);
+            }
+        });
+    };
+    retirarDinero(500)
+        .then(montoActual => console.log(`Me quedan ${montoActual}`))
+        .catch(console.warn);
 })();
