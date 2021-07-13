c7a22826062cc0f88c7150da01f1b1e99aafe8f3
Daryl Olivares <dolivares@marsol.cl>
2021-07-13 17:05:14 -0400

    Promesas 1

--- typescript/app.js
+++ typescript/app.js
@@ -1,43 +1,17 @@
 "use strict";
-(function () {
-    var avenger = {
-        nombre: 'Steve',
-        clave: 'Capitán América',
-        poder: 'SuperSoldado'
-    };
-    var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder; //NO IMPORTA EL ORDEN
-    console.log(nombre);
-    console.log(clave);
-    console.log(poder);
-    var extraer = function (avenger) {
-        var nombre = avenger.nombre, poder = avenger.poder;
-        console.log(nombre);
-        console.log(poder);
-    };
-    extraer(avenger);
-    var extraer2 = function (_a) {
-        var nombre = _a.nombre, poder = _a.poder;
-        console.log(nombre);
-        console.log(poder);
-    };
-    extraer2(avenger); //Aquí la reestructuración ocurre en el argumento, por lo que te ahorras la linea 16 en extraer()
-    //------------------------------------------------------------------------------------------
-    var avengers = ['Thor', 'Ironman', 'Spiderman'];
-    // [] cuando se quiere desestructurar un arreglo, mientras que {} cuando es un objeto
-    // Con los arreglos si debes especificar el orden
-    var avg1 = avengers[0], avg2 = avengers[1], avg3 = avengers[2];
-    console.log(avg1); //Thor
-    console.log(avg2); //Ironman
-    console.log(avg3); //Spiderman
-    //Para omitir entradas en el array, es de la siguiente forma:
-    var avg4 = avengers[2];
-    console.log(avg4); //Spiderman
-    //Para función: 
-    var extraerArr = function (_a) {
-        var av1 = _a[0], av2 = _a[1], av3 = _a[2];
-        console.log(av1); //Thor
-        console.log(av2); //Ironman
-        console.log(av3); //Spiderman
-    };
-    extraerArr(avengers);
+(() => {
+    console.log('Inicio');
+    const prom1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Se terminó el timeout');
+            //reject('Hubo un error');
+        }, 1000);
+    });
+    prom1.then(mensaje => console.log(mensaje)); //Devuelve la respuesta correcta
+    prom1.catch(err => console.warn(err)); //Devuelve el error
+    //Puede ser realizado igualmente de la siguiente forma:
+    prom1
+        .then(mensaje => console.log(mensaje))
+        .catch(err => console.warn(err));
+    console.log('Final');
 })();
